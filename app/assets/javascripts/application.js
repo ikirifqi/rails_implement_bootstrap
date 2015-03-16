f330a4bde2bc4b292de536681a2244502026467a
Rifqi Thefi Fauzi <rifqi.thefi.fauzi@gmail.com>
2015-03-16 14:45:22 +0700

    final push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 //= require_tree .
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def home; end
+
+  def profile; end
+
+  def contact; end
+end
